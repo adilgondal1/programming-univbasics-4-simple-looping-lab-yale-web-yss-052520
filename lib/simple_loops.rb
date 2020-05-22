2d25f7341f8e4dc9fb65a71634cbc4c342693dd0
Adil Gondal <adil.gondal@yale.edu>
2020-05-22 06:21:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,11 @@
 # Write your methods here
+def loop_message_five_times (message)
+  counter = 0
+  while counter < 5 do
+    puts message
+    counter++
+  end
+end
+#loop_message_n_times
+#output_array
+#return_string_array 
