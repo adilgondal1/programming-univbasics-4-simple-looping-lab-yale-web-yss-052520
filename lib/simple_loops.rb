d9f500a10fb0ba9be39016f932f49bd8d97c91a7
Adil Gondal <adil.gondal@yale.edu>
2020-05-22 06:29:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -24,10 +24,11 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  count = 0 
+  count = 0
+  new_array = []
   while count < array.length do
-    array[count] = array[count].to_s
+    new_array.push(array[count].to_s) 
     count += 1
   end
-  array
-end  
+  new_array
+end 
